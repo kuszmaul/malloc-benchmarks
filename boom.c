b31cb897be6c8118f39e1d0857e2f0a275dc7d73
Bradley C. Kuszmaul <kuszmaul@gmail.com>
2026-05-26 10:46:23 -0400

    Record live_data_size and print blowup

--- boom.c
+++ boom.c
@@ -10,6 +10,8 @@ size_t base_rss;
 size_t free_us_length;
 void** free_us;
 
+size_t live_data_size = 0;
+
 size_t get_rss() {
   struct rusage ru;
   int r = getrusage(RUSAGE_SELF, &ru);
@@ -17,8 +19,29 @@ size_t get_rss() {
   return ru.ru_maxrss*1024ul;
 }
 
+size_t get_adjusted_rss() {
+  return get_rss() - base_rss;
+}
+
 void print_rss() {
-  printf("%lu\n", get_rss() - base_rss);
+  size_t rss = get_adjusted_rss();
+  printf("%lu %4.2f\n", rss, (1.0*rss)/live_data_size);
+}
+
+void* my_malloc(size_t size) {
+  live_data_size += size;
+  assert(size >= sizeof(size_t));
+  void *r = malloc(size);
+  memcpy(r, &size, sizeof(size_t));
+  return r;
+}
+
+void my_free(void* address) {
+  size_t size;
+  memcpy(&size, address, sizeof(size_t));
+  assert(size <= live_data_size);
+  live_data_size -= size;
+  free(address);
 }
 
 void first_fit_boom_class(size_t block_size, size_t space) {
@@ -27,7 +50,7 @@ void first_fit_boom_class(size_t block_size, size_t space) {
   size_t free_us_count = 0;
   fprintf(stderr, "Allocating %lu blocks of size %lu\n", n_to_allocate, block_size);
   for (size_t i = 0; i < n_to_allocate; i++) {
-    void* ptr = malloc(block_size);
+    void* ptr = my_malloc(block_size);
     assert(ptr != NULL);
     if (i % 2 == 1) {
       assert(free_us_count < free_us_length);
@@ -46,7 +69,7 @@ void first_fit_boom_class(size_t block_size, size_t space) {
  * block, and allocate blocks of size 16 of total size `space` and free every
  * other block, then blocks of size 32 and so forth. */
 void first_fit_boom(size_t space) {
-  size_t block_size = 8;
+  size_t block_size = 16; // For glibc, the smallest effective object size is 16 bytes.
   while (block_size <= space) {
     first_fit_boom_class(block_size, space);
     block_size *= 2;
@@ -59,7 +82,7 @@ void superblock_boom_class(size_t block_size, size_t space, size_t superblock_si
   size_t free_us_count = 0;
   fprintf(stderr, "Allocating %lu blocks of size %lu\n", n_to_allocate, block_size);
   for (size_t i = 0; i < n_to_allocate; i++) {
-    void* ptr = malloc(block_size);
+    void* ptr = my_malloc(block_size);
     assert(ptr != NULL);
     free_us[free_us_count++] = ptr;
   }
