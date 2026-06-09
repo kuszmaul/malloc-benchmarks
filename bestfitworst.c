a985a10e1810fcad23b9c4c6cf70908a3ab0eeca
Bradley C. Kuszmaul <kuszmaul@gmail.com>
2026-06-09 10:25:58 -0400

    8 repeats

--- bestfitworst.c
+++ bestfitworst.c
@@ -337,7 +337,7 @@ int main(int argc __attribute__((unused)), char **argv __attribute__((unused))) 
   pmfree(3, n-3);
   pmfree(5, n-3);
 
-  const size_t target_n_repeats = 5;
+  const size_t target_n_repeats = 10;
   for (size_t n_repeats = 3; n_repeats < target_n_repeats; n_repeats++) {
     // We start with an `A(n-2)` allocation followed by `n_repeats` of `F(n-3); A1`.
     assert_n_repeats(n_repeats);
@@ -472,5 +472,9 @@ int main(int argc __attribute__((unused)), char **argv __attribute__((unused))) 
     }
   }
   assert_n_repeats(target_n_repeats);
+  struct strbuf buf;
+  printptrs_buf(init_buf(&buf));
+  writes(fd, "Final state\n");
+  mywrite(fd, buf.str, buf.next);
   return 0;
 }
