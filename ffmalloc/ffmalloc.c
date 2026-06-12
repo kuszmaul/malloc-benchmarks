2cf6af4fd55092b6d3b71190a75a5417bc91037f
Bradley C. Kuszmaul <kuszmaul@gmail.com>
2026-06-12 10:47:01 -0400

    More memalign testing

--- ffmalloc/ffmalloc.c
+++ ffmalloc/ffmalloc.c
@@ -16,38 +16,51 @@
 //    containing a header, called the boundary tag. The boundary tag is 1 word containing
 //      ```
 //      struct boundary_tag {
-//          size_t is_overallocated:1;
+//          size_t is_memaligned:1;
 //          size_t size:63;
 //      }}
 //      ```
 //
 //    The meaning is this:
 //
-//      If `is_overallocated` is `false` then `size` is the number of bytes in
-//        the block.  if `size` is >= `mmap_lower_bound` then the block is
-//        mmapped.  The size must be a multiple of `page_size`.
+//      If `is_memaligned` is `false` then `size` is the number of bytes in the
+//        block.  if `size` is >= `mmap_lower_bound` then the block is mmapped.
+//        The size must be a multiple of `page_size`.
 //
-//      If `is_overallocated` is `true` then we this block is the result result
-//        of a call to `posix_memalign` or `aligned_alloc` or `memalign`.  To
-//        allocate a block of size `s` aligned to `a` (which must be a power of
-//        two) we need to allocate extra bytes and then waste part of the
-//        allocation.  The idea is that if we allocate `s + a - 1` bytes, then
-//        somewhere in the resulting block there is a block of size `s` that is
-//        aligned to `a`.  For an overallocated block we need a 2-word header:
-//        The first is a pointer to the actual beginning of the block, and the
-//        second is our header with `is_overallocated==true`.  The `size` is the
-//        size of the originally allocated block.  (And if `size >=
-//        mmap_lower_bound` then the originally allocated block is allocated via
-//        mmap and the size is a multiple of `page_size`.
-
-//   mmap the block
-//   put the boundary tag in word 0 (a word is 8 bytes)
+//      If `is_memaligned` is `true` then we this block is the result of a call
+//        to `posix_memalign` or `aligned_alloc` or `memalign`.  To allocate a
+//        block of size `s` aligned to `a` (which must be a power of two) we
+//        need to allocate extra bytes and then waste part of the allocation.
+//        The idea is that if we allocate `s + a - 1` bytes, then somewhere in
+//        the resulting block there is a block of size `s` that is aligned to
+//        `a`.  For a memaligned block we need a 2-word header: The first is
+//        a pointer to the actual beginning of the block, and the second is our
+//        header with `is_memaligned==true`.  The `size` is the size of the
+//        originally allocated block.  (And if `size >= mmap_lower_bound` then
+//        the originally allocated block is allocated via mmap and the size is a
+//        multiple of `page_size`.)
+//
+//        When allocating a memaligned block, one could imagine peeling off the
+//        unused memory at the beginning of the allocation and making it
+//        available for different allocation.  Doing so could be problematic,
+//        however, sine the resulting block would be in the wrong place for
+//        first fit.  You could end up with a very small block at a very high
+//        address, which would violate the invariant proved in Robson77 (Section
+//        3) that small blocks appear only at low addresses. For memaligned
+//        blocks, the extra block wouldn't even be in the right part of the
+//        address space.  So instead, we simply treat the unused memory as part
+//        of the allocation.
+//
+//        We also special-case alignment==8, since that adds no alignment
+//        constraints: it just directly calls malloc, which always returns
+//        8-byte aligned values.
 
 #include <assert.h>
 #include <errno.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
@@ -73,7 +86,7 @@ static FFTREE *arena = NULL;
 static size_t last_sbrk_size = 1;
 
 typedef struct boundary_tag {
-  size_t is_overallocated : 1;
+  size_t is_memaligned : 1;
   size_t size : 63; // including the boundary tag and any unused space at the end
 
   // For munaligned mmapped blocks, the pointer we give the user points at a
@@ -88,12 +101,16 @@ static BOUNDARY_TAG* get_boundary_tag_pointer(void *p) {
   return ((BOUNDARY_TAG*)(p)) - 1;
 }
 
-static void** get_overallocated_original_stored_at(void *p) {
+static void** get_memaligned_original_stored_at_pointer(void *p) {
   return ((void**)(get_boundary_tag_pointer(p))) - 1;
 }
 
 static BOUNDARY_TAG get_boundary_tag(void *p) {
   return *(get_boundary_tag_pointer(p));
+}
+
+static void* get_memaligned_original(void *p) {
+  return *get_memaligned_original_stored_at_pointer(p);
 }
 
 // For debugging
@@ -173,12 +190,13 @@ static int ff_malloc_mmap_e(void** result, size_t size) {
   size = alignup(size + sizeof(BOUNDARY_TAG), page_size);
   assert(size >= mmap_lower_bound);
   void* p = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  fprintf(stderr, "mmap(0, %lu, ...) => %p\n", size, p);
   if (p == (void*)-1) {
     assert(errno == ENOMEM);
     return ENOMEM;
   }
   BOUNDARY_TAG *bt = p;
-  bt->is_overallocated = false;
+  bt->is_memaligned = false;
   bt->size = size;
   *result = (void*)((char*)p + sizeof(BOUNDARY_TAG));
   return 0;
@@ -191,13 +209,13 @@ static int ff_malloc_mmap_e(void** result, size_t size) {
 /*   assert(p); */
 /*   { */
 /*     BOUNDARY_TAG *bt = p; */
-/*     bt->is_overallocated = false; */
+/*     bt->is_memaligned = false; */
 /*     bt->size = mmaped_size; */
 /*   } */
 /*   char *returned_pointer = (char*)(alignup((uintptr_t)p + 2*sizeof(BOUNDARY_TAG), alignment)); */
 /*   { */
 /*     BOUNDARY_TAG *bt = returned_pointer - sizeof(BOUNDARY_TAG); */
-/*     bt->is_overallocated = true; */
+/*     bt->is_memaligned = true; */
 /*     bt->size = */
 
 
@@ -256,7 +274,7 @@ static int ff_malloc_firstfit_e(void **result, size_t size) {
   }
   size_t nsize = node->size;
   BOUNDARY_TAG* tag = (BOUNDARY_TAG*)(node);
-  tag->is_overallocated = false;
+  tag->is_memaligned = false;
   tag->size = nsize;
   *result = (void*)((char*)node + sizeof(BOUNDARY_TAG));
   return 0;
@@ -292,16 +310,21 @@ static void fftree_print(FFTREE *tree, int indent) {
 }
 
 static int ff_posix_memalign(void **result, size_t alignment, size_t size) {
-  assert(ispow2(alignment));
+  if (!ispow2(alignment)) return EINVAL;
   assert(alignment % sizeof(void*) == 0);
   if (size == 0) {
     *result = NULL;
     return 0;
   }
+  if (alignment <= sizeof(BOUNDARY_TAG)) {
+    // small alignments don't need anything
+    return ff_malloc_e(result, size);
+  }
   size_t amount_to_malloc = sizeof(BOUNDARY_TAG) + size + alignment - 1;
   void *p;
   int r = ff_malloc_e(&p, amount_to_malloc);
   if (r != 0) return r;
+  fprintf(stderr, "ff_malloc returned =%p\n", p);
   //fprintf(stderr, "ff_malloc got %p\n", p);
   // We now have
   //   p[-8] the original boundary tag (which we won't use)
@@ -313,9 +336,9 @@ static int ff_posix_memalign(void **result, size_t alignment, size_t size) {
   BOUNDARY_TAG *new_tag_p = get_boundary_tag_pointer(p_to_return);
   *new_tag_p = (BOUNDARY_TAG){1, original_boundary_tag.size};
   //fprintf(stderr, "new_tag_p=%p\n", new_tag_p);
-  //fprintf(stderr, "new_tag={%d %lu}\n", new_tag_p->is_overallocated, (size_t)(new_tag_p->size));
-  void ** store_start_at = get_overallocated_original_stored_at(p_to_return);
-  //  fprintf(stderr, "store_start_at=%p\n",store_start_at);
+  //fprintf(stderr, "new_tag={%d %lu}\n", new_tag_p->is_memaligned, (size_t)(new_tag_p->size));
+  void ** store_start_at = get_memaligned_original_stored_at_pointer(p_to_return);
+  fprintf(stderr, "store_start_at=%p\n",store_start_at);
   *store_start_at = get_boundary_tag_pointer(p);
   *result = p_to_return;
   return 0;
@@ -323,7 +346,7 @@ static int ff_posix_memalign(void **result, size_t alignment, size_t size) {
 
 static void ff_free(void *p) {
   BOUNDARY_TAG bt = get_boundary_tag(p);
-  if (!bt.is_overallocated) {
+  if (!bt.is_memaligned) {
     if (bt.size >= mmap_lower_bound) {
       BOUNDARY_TAG* btp = get_boundary_tag_pointer(p);
       assert(((uintptr_t)(btp)) % page_size == 0);
@@ -333,12 +356,25 @@ static void ff_free(void *p) {
       assert(0); // not ready
     }
   } else {
-    BOUNDARY_TAG *original_tag_p = (BOUNDARY_TAG*)(*(get_overallocated_original_stored_at(p)));
+    // Reconstruct the boundary tag as it was originally before we did the
+    // memalignment hacking.  (We could have overwritten the boundary tag with
+    // the original_stored_at information.)
+    BOUNDARY_TAG *original_tag_p = (BOUNDARY_TAG*)(*(get_memaligned_original_stored_at_pointer(p)));
     fprintf(stderr, "freeing original_tag_p=%p\n", original_tag_p);
     *original_tag_p = (BOUNDARY_TAG){0, bt.size};
-    fprintf(stderr, "fixed up boundary tag={%lu %lu}\n", (size_t)(original_tag_p->is_overallocated), (size_t)(original_tag_p->size));
+    fprintf(stderr, "fixed up boundary tag={%lu %lu}\n", (size_t)(original_tag_p->is_memaligned), (size_t)(original_tag_p->size));
     ff_free(original_tag_p + 1);
   }
+}
+
+static void my_mincore(void *addr, size_t length, unsigned char *vec) {
+  fprintf(stderr, "mincore(%p, %lu, ...) => ",  addr, length);
+  int r = mincore((void*)(addr), length, vec);
+  assert(r == 0);
+  for (size_t i = 0; i < (length + page_size - 1)/page_size; i++) {
+    fprintf(stderr, "%u", vec[i] & 1);
+  }
+  fprintf(stderr, "\n");
 }
 
 // Tester
@@ -357,7 +393,7 @@ static void test_big_malloc(void) {
   assert(r==0);
   assert(((uintptr_t)p) % page_size == 8);
   BOUNDARY_TAG bt = ((BOUNDARY_TAG*)(p))[-1];
-  assert(!bt.is_overallocated);
+  assert(!bt.is_memaligned);
   fprintf(stderr, "requested size = %d\n", 2*mmap_lower_bound);
   fprintf(stderr, "bt.size=%lu\n", (size_t)(bt.size));
   assert(bt.size == 2*mmap_lower_bound+page_size);
@@ -365,23 +401,80 @@ static void test_big_malloc(void) {
   ff_free(p);
 }
 
-static void test_big_posix_memalign(void) {
-  fprintf(stderr, "\n%s\n", __FUNCTION__);
+static void test_big_posix_memalign_errors(void) {
+  errno = 0;
+  size_t size = 2*mmap_lower_bound;
   void *result;
-  int r = ff_posix_memalign(&result, 32, 2*mmap_lower_bound);
-  assert(r==0);
-  fprintf(stderr, "result=%p\n", result);
-  assert((uintptr_t)(result)%32 == 0);
-  BOUNDARY_TAG bt = ((BOUNDARY_TAG*)(result))[-1];
-  fprintf(stderr, "bt={%d %lu}\n", bt.is_overallocated, (size_t)(bt.size));
-  assert(bt.is_overallocated);
+  // alignments that are smaller than void* are not allowed.
+  for (size_t alignment = 0; alignment < sizeof(void*); alignment++) {
+    int r = ff_posix_memalign(&result, 0, size);
+    assert(r == EINVAL);
+  }
+  // alignments that are not a power of two are not allowed.
+  {
+    int r = ff_posix_memalign(&result, sizeof(void*)+1, size);
+    assert(r == EINVAL);
+  }
+  // Too much is not allowed
+  {
+    int r = ff_posix_memalign(&result, page_size, 1ul<<63);
+    assert(r == ENOMEM);
+  }
+}
 
+static void test_big_posix_memalign(size_t alignment) {
+  fprintf(stderr, "\n%s(%lu)\n", __FUNCTION__, alignment);
+  void *result;
+  {
+    int r = ff_posix_memalign(&result, alignment, 2*mmap_lower_bound);
+    assert(r==0);
+  }
+  fprintf(stderr, "result=%p\n", result);
+  assert((uintptr_t)(result)%alignment == 0);
+  BOUNDARY_TAG bt = get_boundary_tag(result);
+  fprintf(stderr, "bt={%d %lu}\n", bt.is_memaligned, (size_t)(bt.size));
+  uintptr_t result_beginning = ((uintptr_t)(result)) & ~(page_size-1);
+  if (alignment == sizeof(void*)) {
+    assert(!bt.is_memaligned);
+  } else {
+    assert(bt.is_memaligned);
+    fprintf(stderr, "memaligned original = %p\n", get_memaligned_original(result));
+    assert(get_memaligned_original(result) == (void*)(result_beginning));
+  }
+
+  size_t expected_mmap_size = (2*mmap_lower_bound+page_size);
+  unsigned char mincore_result[expected_mmap_size/page_size];
+  assert(result_beginning % page_size == 0);
+  fprintf(stderr, "result_beginning=%p\n", (void*)(result_beginning));
+  my_mincore((void*)(result_beginning), expected_mmap_size, mincore_result);
+  assert((mincore_result[0] & 1) == 1);
+  for (size_t i = 1; i < expected_mmap_size/page_size; i++) {
+    assert((mincore_result[i] & 1) == 0);
+  }
+  memset(result, 1, 2*mmap_lower_bound);
+  my_mincore((void*)(result_beginning), expected_mmap_size, mincore_result);
+  {
+    bool ok = true;
+    for (size_t i = 0; i < expected_mmap_size/page_size; i++) {
+      if ((mincore_result[i] & 1u) == 0) {
+        fprintf(stderr, "page %lu not in core\n", i);
+        ok = false;
+      }
+    }
+    assert(ok);
+  }
   ff_free(result);
 }
 
 int main(int argc __attribute__((unused)), char **argv __attribute__((unused))) {
   test1();
   test_big_malloc();
-  test_big_posix_memalign();
+  test_big_posix_memalign_errors();
+  // alignment of size 8 adds no additional requirements
+  test_big_posix_memalign(sizeof(void*));
+  // alignment  size 16
+  test_big_posix_memalign(2*sizeof(void*));
+  // alignment  size 32
+  test_big_posix_memalign(4*sizeof(void*));
   return 0;
 }
