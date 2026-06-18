bf5fc3f79cd2ab6e8a7bd6740505d0d28c004de8
Bradley C. Kuszmaul <kuszmaul@gmail.com>
2026-06-18 09:59:13 -0400

    bumpmalloc works.  It turns out that assert was causing trouble

--- ffmalloc/bumpmalloc.c
+++ ffmalloc/bumpmalloc.c
@@ -2,12 +2,55 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-enum {DSIZE = 1<<20};
-char data[DSIZE];
+#include "max.h"
+
+char *data = NULL;
 size_t free_index = 0;
+size_t data_size = 0;
+size_t last_sbrk_size = 0;
+
+static void do_sbrk(size_t n) {
+  // We generally can sbrk a huge amount of memory, but we cannot do it more
+  // than a few gigabytes at a time.
+  const size_t max_single_sbrk_size = 1<<24;
+  while (n > 0) {
+    size_t m = max(max_single_sbrk_size, n);
+    void *r = sbrk(m);
+    if (r == (void*)-1) {
+      write(2, "sbrk failed\n", 12);
+      abort();
+    }
+    data_size += m;
+    n -= m;
+  }
+}
+
+static void ensure_space(size_t n) {
+  // Make sure there are n bytes free.
+  if (n <= data_size - free_index) return;
+
+  if (data == NULL) {
+    data = sbrk(0);
+    if (data == (void*)-1) {
+      write(2, "sbrk(0) failed\n", 12);
+      abort();
+    }
+    free_index = 0;
+  }
+  last_sbrk_size = max(n, last_sbrk_size) + last_sbrk_size;
+  do_sbrk(last_sbrk_size);
+
+  data_size += last_sbrk_size;
+
+  if (n > data_size - free_index) {
+    write(2, "data size not good\n", 19);
+    abort();
+  }
+}
 
 void *malloc(size_t n) {
-  assert(free_index + n <= DSIZE);
+  if (n == 0) return NULL;
+  ensure_space(n);
   char *result = data+free_index;
   free_index += n;
   return result;
