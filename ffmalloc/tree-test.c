260ccbd33a901678ef2d99d8609c52e18b21e0d8
Bradley C. Kuszmaul <kuszmaul@gmail.com>
2026-06-22 12:48:37 -0400

    Tests pass

--- ffmalloc/tree-test.c
+++ ffmalloc/tree-test.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>  // use a known-working version of malloc to run these tests.
 #include <string.h>
 
+#include "headers.h"
 #include "max.h"
 #include "tree.h"
 #include "tree-test-helpers.h"
